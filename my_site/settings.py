247429c04f1b4583cfbb371a05e7bce3013be591
Olamidun <kolapoolamidun@gmail.com>
2020-08-31 21:35:32 +0100

    installed django-heroku

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,5 @@ STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'polls:home'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
